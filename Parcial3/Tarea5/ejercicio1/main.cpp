721f57f0a03adc8b44db59db716445644a8b490f
MauriceGS <Maugssw@gmail.com>
2015-04-23 21:40:08 -0500

    Create main.cpp

---
+++ Parcial3/Tarea5/ejercicio1/main.cpp
@@ -0,0 +1,37 @@
+#include "BinaryTree.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace vcn;
+
+int main(){
+
+BinaryTree<int> arbol;
+
+BNode<int> * primero=new BNode<int>(2);
+BNode<int> * segundo=new BNode<int>(2);
+BNode<int> * tercero=new BNode<int>(2);
+BNode<int> * cuarto=new BNode<int>(2);
+BNode<int> * quinto=new BNode<int>(2);
+BNode<int> * sexto=new BNode<int>(2);
+
+
+arbol.insert(nullptr,primero);
+arbol.insert(primero,segundo);
+arbol.insert(primero,tercero);
+arbol.insert(segundo,cuarto);
+arbol.insert(segundo,quinto);
+arbol.insert(tercero,sexto);
+
+int suma;
+
+arbol.preOrder();
+cout<<endl;
+
+arbol.sumaNodos();
+
+arbol.preOrder();
+return 0;
+
+}
