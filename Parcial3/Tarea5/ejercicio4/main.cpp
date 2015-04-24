7dd9d82a84c1bb872c834daa16876a8bf63ef132
MauriceGS <Maugssw@gmail.com>
2015-04-23 23:04:15 -0500

    Create main.cpp

---
+++ Parcial3/Tarea5/ejercicio4/main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <string.h>
+#include "Persona.h"
+#include "BinaryTree.h"
+using namespace vcn;
+using namespace std;
+
+BinaryTree<Persona> arbol;
+
+void agregarMiembro();
+void verDescendientes();
+void verParientes();
+void verAncestros();
+void fallecido();
+void verFallecidos();
+void verVivos();
+
+int main(){
+
+
+void agregarMiembro(){
+
+    string padre,miembro,apellidoP,apellidoM;
+    int edadP,edadM;
+
+
+    cout << " Cual es el nombre del padre ? ";
+    cin >> padre;
+    cout << " Cual es el apellido del padre ? ";
+    cin >> apellidoP;
+    cout << " Cual es la edad del padre ? ";
+    cin >> edadP;
+    cout << " Cual es el nombre del miembro ? ";
+    cin >> miembro;
+    cout << " Cual es el apellido del miembro ? ";
+    cin >> apellidoM;
+    cout << " Cual es la edad del miembro ? ";
+    cin >> edadM;
+
+    Persona padre(padre, apellidoP,edadP);
+    Persona miembro(miembro, apellidoM, edadM);
+
+
+    BNode<Persona> * nodoPadre = arbol.search(padre);
+    BNode<Persona> * nodoMiembro = new BNode<Persona>(hijo);
+
+    arbol.insert(nodoPadre,nodoMiembro);
+    arbol.preOrder();
+
+
+}
+void verDescendientes(){
+
+    string padre,apellidoP;
+    int edadP;
+
+    cout << " Cual es el nombre del padre ? ";
+    cin >> padre;
+    cout << " Cual es el apellido del padre ? ";
+    cin >> apellidoP;
+    cout << " Cual es la edad del padre ? ";
+    cin >> edadP;
+
+    Persona padre(padre, apellidoP,edadP);
+
+    BNode<Persona> * origen = arbol.search(padre);
+
+    arbol.preOrder(origen);
+
+
+    }
+
+void verParientes(){
+string miembro,apellidoM;
+int edadM;
+
+    cout << " Cual es el nombre del miembro ? ";
+    cin >> miembro;
+    cout << " Cual es el apellido del miembro ? ";
+    cin >> apellidoM;
+    cout << " Cual es la edad del miembro ? ";
+    cin >> edadM;
+
+    Persona miembro(miembro, apellidoM, edadM);
+
+    BNode<Persona> * pariente = arbol.search(miembro);
+
+    arbol.verParientes(pariente);
+
+
+}
+void verAncestros(){
+ string miembro,apellidoM;
+ int edadM;
+
+    cout << " Cual es el nombre del miembro ? ";
+    cin >> miembro;
+    cout << " Cual es el apellido del miembro ? ";
+    cin >> apellidoM;
+    cout << " Cual es la edad del miembro ? ";
+    cin >> edadM;
+
+    Persona miembro(miembro, apellidoM, edadM);
+
+    BNode<Persona> * origen= arbol.search(miembro);
+
+    arbol.ancestors(miembro);
+
+
+
+}
+void fallecido(){
+
+   string miembro,apellidoM;
+   int edadM,fecha;
+
+    cout << " Cual es el nombre del miembro fallecido? ";
+    cin >> miembro;
+    cout << " Cual es el apellido del miembro fallecido ? ";
+    cin >> apellidoM;
+    cout << " Cual es la edad del miembro fallecido? ";
+    cin >> edadM;
+    cout << " Cual es la fecha del fallecimiento? ";
+    cin >> fecha;
+
+    Persona miembro(miembro, apellidoM, edadM);
+
+    BNode<Persona> * fallecidoN = arbol.search(miembro);
+
+    Persona fallecido = fallecidoN->getInfo();
+    fallecido.setFallecimiento(true);
+    fallecido.setFecha(fecha);
+
+
+    fallecidoN.setInfo(fallecido);
+
+}
+void verFallecidos(){
+int fecha;
+cout << "A partir de cual fecha gusta ver los fallecimientos ?";
+cin >> fecha;
+
+}
+void verVivos(){
+
+int fecha;
+cout << "A partir de cual fecha gusta ver los miembros vivos ?";
+cin >> fecha;
+
+}
+
+
+
+
+
+
+
+
+return 0;
+}
