fe8f909718899df418868aa97d1666b83f2982ad
MauriceGS <Maugssw@gmail.com>
2015-04-23 21:54:32 -0500

    Update main.cpp

--- Parcial3/Tarea5/ejercicio3/main.cpp
+++ Parcial3/Tarea5/ejercicio3/main.cpp
@@ -1,1 +1,30 @@
 
+#include "BinaryTree.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace vcn;
+
+int main(){
+BinaryTree<int> arbol;
+
+BNode<int> * primero=new BNode<int>(2);
+BNode<int> * segundo=new BNode<int>(2);
+BNode<int> * tercero=new BNode<int>(2);
+BNode<int> * cuarto=new BNode<int>(2);
+BNode<int> * quinto=new BNode<int>(2);
+BNode<int> * sexto=new BNode<int>(2);
+
+arbol.insert(nullptr,primero);
+arbol.insert(primero,segundo);
+arbol.insert(primero,tercero);
+arbol.insert(segundo,cuarto);
+arbol.insert(segundo,quinto);
+arbol.insert(tercero,sexto);
+
+cout<<"Arbol Ordenado"<<endl
+arbol.preOrder();
+
+return 0;
+}
